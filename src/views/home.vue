0fc0fb5acd1d034c5e77aabc3a56fb11e31e2967
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-01-08 00:19:02 +0200

    Adding view folder for each page in the app

---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-flex xs6>
+      <TodoForm />
+      <TodoList/>
+    </v-flex>
+  </div>
+</template>
+<script>
+import TodoForm from '@/components/todo-form'
+import TodoList from '@/components/todo-list'
+export default {
+    name: 'Home',
+  components: {
+    TodoForm,
+    TodoList,
+  }
+}
+</script>
