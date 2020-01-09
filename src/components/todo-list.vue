4f715ce312e7b311b49a742752e2e5a65ad188d2
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-01-09 21:34:55 +0200

    display all tasks from vuex orm

--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -1,16 +1,24 @@
 <template lang="html">
   <div>
-    <TodoItem class="mb-3" />
-    <TodoItem class="mb-3"/>
+    <TodoItem 
+      v-for="task in tasks" 
+      :key="task.id" 
+      :task="task" 
+      class="mb-3" />
   </div>
 </template>
 
 <script>
 import TodoItem from '@/components/todo-item';
-
+import task from '@/classes/task'
 export default {
   components: {
     TodoItem,
+  },
+  computed:{
+    tasks(){
+      return task.all();
+    }
   }
 }
 </script>
