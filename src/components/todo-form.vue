c545c4f206afba4ad5469afc5233838c4811cbed
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-01-09 21:34:03 +0200

    Add validation for inserting task

--- src/components/todo-form.vue
+++ src/components/todo-form.vue
@@ -1,13 +1,45 @@
 <template lang="html">
-  <v-text-field
-    label="Todo"
-    placeholder="What do you have to do?"
-    solo
-  />
+  <v-form 
+    ref="form" 
+    v-model="valid" 
+    @submit="insertTask" 
+    lazy-validation>
+    <v-text-field 
+      label="Todo" 
+      placeholder="What do you have to do?" 
+      v-model="title"
+      :rules="rules.required"
+      solo />
+  </v-form>
 </template>
 
 <script>
+import task from '../classes/task'
 export default {
+    data() {
+        return {
+            valid: true,
+            title: '',
+                  rules: {
+                required: [
+                    v => !!v || 'Field is required',
+                ]
+            }
+        }
+    },
+    methods: {
+        insertTask(e) {
+            e.preventDefault();
+            if (this.$refs.form.validate()) {
+                task.insert({
+                    data: {
+                        title: this.title
+                    }
+                })
+                this.$refs.form.reset()
+            }
+        }
+    },
 }
 </script>
 
