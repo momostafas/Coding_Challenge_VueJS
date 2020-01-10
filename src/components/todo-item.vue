467291c0653866291641e48975dead9f6b93fab9
Mohamed Mostafa <mo.mostafa@nu.edu.eg>
2020-01-10 16:00:12 +0200

    add confirmation dialog before deleting task

--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -18,12 +18,33 @@
     >
       Complete
     </v-btn>
-    <v-btn
-      color="error"
-      @click="deleteTask(task.id)"
-    >
-      Delete
-    </v-btn>
+    <v-dialog 
+      v-model="deleteDialog" 
+      persistent 
+      max-width="290">
+      <v-btn 
+        slot="activator" 
+        color="error">
+        Delete
+      </v-btn>
+      <v-card>
+        <v-card-title class="headline">Confirm Deletion</v-card-title>
+        <v-card-text>Delete this task ?</v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn 
+            color="green darken-1" 
+            flat 
+            outline 
+            @click.native="deleteDialog = false">Cancel</v-btn>
+          <v-btn 
+            color="error darken-1" 
+            flat 
+            outline 
+            @click="deleteTask(task.id)">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -31,6 +52,11 @@
 import VueTypes from 'vue-types'
 import task from '@/classes/task'
 export default {
+  data() {
+    return {
+                  deleteDialog: false,
+    }
+  },
     props: {
         task: VueTypes.object
     },
